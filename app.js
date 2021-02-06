ae6fe1746cb7bfa914031cdbd68235f44a0e8a51
fogset <zhangth1990@hotmail.com>
2021-02-05 17:57:04 -0800

    setting up home page

--- app.js
+++ app.js
@@ -16,18 +16,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/", function(req, res) {
+  res.render("home");
+})
 
 
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
