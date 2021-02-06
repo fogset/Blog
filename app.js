6b400b2093fb8ac85530cf5e6f8aa12df4f8ecf1
fogset <zhangth1990@hotmail.com>
2021-02-06 01:55:34 -0800

    console log the input of the published data

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ app.get("/contact", function(req, res) {
 app.get("/compose", function(req, res) {
   res.render("compose");
 })
+app.post("/compose", function(req, res) {
+  let composeInput = req.body.composeInput;
+  console.log(composeInput);
+})
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
