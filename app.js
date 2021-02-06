ae099b7eaec1c8329b8b42636cfab1cd1205793a
fogset <zhangth1990@hotmail.com>
2021-02-06 01:40:46 -0800

    set up the compose page

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.get("/contact", function(req, res) {
     contactContent: contactContent,
   });
 })
+app.get("/compose", function(req, res) {
+  res.render("compose");
+})
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
